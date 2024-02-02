689e4d5f7d481d53987c15eaa9b2b67aa7744d9a
newbin4126 <dkfmal710@gmail.com>
2024-02-02 20:31:04 +0900

    notion-clone day4

--- notion-clone/app/(main)/_components/navbar.tsx
+++ notion-clone/app/(main)/_components/navbar.tsx
@@ -4,49 +4,57 @@ import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
+import { Menu } from "./menu";
 import { MenuIcon } from "lucide-react";
+
 import { Title } from "./title";
 import { Banner } from "./banner";
 
 interface NavbarProps {
-  isCollapsed: boolean;
-  onResetWidth: () => void;
+    isCollapsed: boolean;
+    onResetWidth: () => void;
 }
 
 export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
-  const params = useParams();
+    const params = useParams();
 
-  const document = useQuery(api.documents.getById, {
-    documentId: params.documentId as Id<"documents">,
-  });
+    const document = useQuery(api.documents.getById, {
+        documentId: params.documentId as Id<"documents">,
+    });
 
-  if (document === undefined) {
+    if (document === undefined) {
+        return (
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+                <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
+            </nav>
+        );
+    }
+
+    if (document === null) {
+        return null;
+    }
+
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
-        <Title.Skeleton />
-      </nav>
+        <>
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+                {isCollapsed && (
+                    <MenuIcon
+                        role="button"
+                        onClick={onResetWidth}
+                        className="h-6 w-6 text-muted-foreground"
+                    />
+                )}
+                <div className="flex items-center justify-between w-full">
+                    <Title initialData={document} />
+                    <div className="4flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
+                </div>
+            </nav>
+            {document.isArchived && <Banner documentId={document._id} />}
+        </>
     );
-  }
-
-  if (document === null) {
-    return null;
-  }
-
-  return (
-    <>
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
-        {isCollapsed && (
-          <MenuIcon
-            role="button"
-            onClick={onResetWidth}
-            className="h-6 w-6 text-muted-foreground"
-          />
-        )}
-        <div className="flex items-center justify-between w-full">
-          <Title initialData={document} />
-        </div>
-      </nav>
-      {document.isArchived && <Banner documentId={document._id} />}
-    </>
-  );
 };
---
+++ notion-clone/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["file.adgestore.dev"],
+    },
+};
+
+module.exports = nextConfig;
